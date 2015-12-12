c2580ffb449cf96cdaf2b0ff43da0460a6972401
Simon Li <li@siutsin.com>
2015-12-12 19:19:36 +0800

    update game logic

---
+++ Assets/LevelManager.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+function LoadLevel (name: String) {
+	Debug.Log("Level: " + name);
+	Application.LoadLevel(name);
+}
+
+function QuitRequest () {
+	Debug.Log("I want to quit");
+	Application.Quit();
+}
---
+++ Assets/NumberWizard.js
@@ -0,0 +1,51 @@
+﻿#pragma strict
+
+import UnityEngine.UI;
+
+private var MAX_ALLOWED: int = 1000+1;
+private var MIN_ALLOWED: int = 1;
+
+private var max: int;
+private var min: int;
+private var guess: int;
+
+public var maxGuessesAllowed: int = 10;
+public var text: Text;
+
+private function Start() {
+  StartGame();
+}
+
+private function StartGame() {
+  max = MAX_ALLOWED;
+  min = MIN_ALLOWED;
+  guess = 500;
+  max++;
+}
+
+private function NextGuess() {
+  guess = Random.Range(min, max+1);
+  if (guess == MAX_ALLOWED || guess == MIN_ALLOWED) {
+  	Application.LoadLevel("Lose");
+  }
+  text.text = guess.ToString();
+  maxGuessesAllowed--;
+  if (maxGuessesAllowed <= 0) {
+  	Application.LoadLevel("Win");
+  }
+
+}
+
+public function GuessHigher() {
+	min = guess;
+	NextGuess();
+}
+
+public function GuessLower() {
+	max = guess;
+    NextGuess();
+}
+
+public function Correct() {
+	Application.LoadLevel("Lose");
+}
